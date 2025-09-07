4a9ddef661f8be5e2ea4b0335b607ff647c020b1
rojasebastian <se.huaiquimilla@duocuc.cl>
2025-09-06 21:53:15 -0400

    actualizar

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const video = document.getElementById("bg-video-p");
+
+document.addEventListener("mousemove", (e) => {
+  let x = (e.clientX / window.innerWidth - 0.5) * 80;  // -5% a +5%
+  let y = (e.clientY / window.innerHeight - 0.5) * 80; // -5% a +5%
+
+  video.style.transform = `translate(${x}px, ${y}px) scale(1.05)`;
+});
