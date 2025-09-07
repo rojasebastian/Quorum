96b033b005caf6ba52e5acc44d94f8387685b1e9
rojasebastian <se.huaiquimilla@duocuc.cl>
2025-09-06 22:45:42 -0400

    actualizar

--- js/app.js
+++ js/app.js
@@ -4,5 +4,5 @@ document.addEventListener("mousemove", (e) => {
   let x = (e.clientX / window.innerWidth - 0.5) * 80;  // -5% a +5%
   let y = (e.clientY / window.innerHeight - 0.5) * 80; // -5% a +5%
 
-  video.style.transform = `translate(${x}px, ${y}px) scale(1.05)`;
+  video.style.transform = `translate(calc(-50% + ${x}px), calc(-50% + ${y}px)) scale(1.05)`;
 });
